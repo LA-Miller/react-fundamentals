4579f7cd25fb7bc4bbb3ad366fa067ab36fb8f16
LA-Miller <lamiller516@gmail.com>
2021-11-09 19:29:15 -0500

    First commit

---
+++ react-fundamentals/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
